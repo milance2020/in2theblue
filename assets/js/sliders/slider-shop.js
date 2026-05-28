57a212d0288f26c37074abe2222e5f533e96d51f
Milan Čelebić <2020celebicmilan@gmail.com>
2026-05-28 22:44:06 +0200

    added new sliders for mobile version of site + changed the js for it

--- assets/js/sliders/slider-shop.js
+++ assets/js/sliders/slider-shop.js
@@ -1,22 +1,39 @@
-const slides = [
-    'assets/images/images_shop/materials/slider.png',
-   
-];
-
 const slider = document.getElementById('slider');
 
+const isMobile = window.innerWidth <= 768;
+
+const slides = isMobile
+    ? [
+        'assets/images/images_shop/materials/slidermob.png',
+    ]
+    : [
+        'assets/images/images_shop/materials/slider.png',
+    ];
+
 slides.forEach((src, i) => {
+
     const img = document.createElement('img');
-    img.src = src;
-    img.style.display = i === 0 ? 'block' : 'none'; // samo prvi slajd vidljiv
+
+    img.src = src + '?v=2';
+
+    img.style.display = i === 0 ? 'block' : 'none';
+
     slider.appendChild(img);
+
 });
 
-// jednostavan slideshow
+
+// SLIDESHOW
 let current = 0;
+
 setInterval(() => {
+
     const images = slider.querySelectorAll('img');
+
     images[current].style.display = 'none';
+
     current = (current + 1) % images.length;
+
     images[current].style.display = 'block';
+
 }, 3000);
